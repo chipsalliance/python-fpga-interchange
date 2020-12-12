56cec97f9bb67a389e0b7e1f7eed030e49fc03ef
Keith Rothman <537074+litghost@users.noreply.github.com>
2020-12-11 16:29:24 -0800

    Update per comments.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-export CAPNP_PATH="$GITHUB_WORKSPACE/env/capnproto-java/compiler/src/main/schema/"
-export INTERCHANGE_SCHEMA_PATH="$GITHUB_WORKSPACE/env/RapidWright/interchange"
 export RAPIDWRIGHT_PATH="$GITHUB_WORKSPACE/env/RapidWright"
 
 # Create the device resource for the test part.
-pushd "$GITHUB_WORKSPACE/env" && \
-    "$RAPIDWRIGHT_PATH/scripts/invoke_rapidwright.sh" \
+pushd "$GITHUB_WORKSPACE/env"
+"$RAPIDWRIGHT_PATH/scripts/invoke_rapidwright.sh" \
     com.xilinx.rapidwright.interchange.DeviceResourcesExample \
-    xc7a50tfgg484-1 && popd
+    xc7a50tfgg484-1
+popd
+
+export CAPNP_PATH="$GITHUB_WORKSPACE/env/capnproto-java/compiler/src/main/schema/"
+export INTERCHANGE_SCHEMA_PATH="$GITHUB_WORKSPACE/env/RapidWright/interchange"
 export DEVICE_RESOURCE_PATH="$GITHUB_WORKSPACE/env"
+
 make test-py
 test $(git status --porcelain | wc -l) -eq 0 || { git diff; false; }
