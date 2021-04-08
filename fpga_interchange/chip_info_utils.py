87b8c1064ad019f4fde168b6525dea3e881e30b5
Alessandro Comodi <acomodi@antmicro.com>
2021-04-08 16:52:39 +0200

    chip_info_utils: added utility classes to store LUT information
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

---
+++ fpga_interchange/chip_info_utils.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021  The SymbiFlow Authors.
+#
+# Use of this source code is governed by a ISC-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/ISC
+#
+# SPDX-License-Identifier: ISC
+
+
+class LutCell():
+    def __init__(self):
+        self.name = ''
+        self.pins = []
+
+
+class LutBel():
+    def __init__(self):
+        self.name = ''
+        self.pins = []
+        self.low_bit = 0
+        self.high_bit = 0
+        self.out_pin = ''
+
+
+class LutElement():
+    def __init__(self):
+        self.width = 0
+        self.lut_bels = []
