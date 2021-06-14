fdc647b69b43f4396cef44c7987902f5ab2d37ee
Maciej Dudek <mdudek@antmicro.com>
2021-06-14 20:14:04 +0200

    Add nextpnr_emit to testarch_device testing
    Signed-off-by: Maciej Dudek <mdudek@antmicro.com>

--- .github/workflows/testarch_device.sh
+++ .github/workflows/testarch_device.sh
@@ -4,3 +4,4 @@ export INTERCHANGE_SCHEMA_PATH="$GITHUB_WORKSPACE/env/fpga-interchange-schema/in
 export CAPNP_PATH="$GITHUB_WORKSPACE/env/capnproto-java/compiler/src/main/schema/"
 
 python3 fpga_interchange/testarch_generators/generate_testarch.py --schema_dir $INTERCHANGE_SCHEMA_PATH
+python3 fpga_interchange/nextpnr_emit.py --schema_dir $INTERCHANGE_SCHEMA_PATH --output_dir . --device_config test_data/gen_device_config.yaml
