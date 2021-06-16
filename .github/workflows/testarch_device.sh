f0334e0e730bf64c3cbb29bdc7e02c825041329c
Maciej Dudek <mdudek@antmicro.com>
2021-06-16 10:05:46 +0200

    Fix testarch_test.sh
    Signed-off-by: Maciej Dudek <mdudek@antmicro.com>

--- .github/workflows/testarch_device.sh
+++ .github/workflows/testarch_device.sh
@@ -4,4 +4,7 @@ export INTERCHANGE_SCHEMA_PATH="$GITHUB_WORKSPACE/env/fpga-interchange-schema/in
 export CAPNP_PATH="$GITHUB_WORKSPACE/env/capnproto-java/compiler/src/main/schema/"
 
 python3 fpga_interchange/testarch_generators/generate_testarch.py --schema_dir $INTERCHANGE_SCHEMA_PATH
-python3 fpga_interchange/nextpnr_emit.py --schema_dir $INTERCHANGE_SCHEMA_PATH --output_dir . --device_config test_data/gen_device_config.yaml --device device_resources.device.gz
+mv device_resources.device.gz $GITHUB_WORKSPACE/env
+python3 fpga_interchange/nextpnr_emit.py --schema_dir $INTERCHANGE_SCHEMA_PATH --output_dir $GITHUB_WORKSPACE/env --device_config test_data/gen_device_config.yaml --device $GITHUB_WORKSPACE/env/device_resources.device.gz
+
+test $(git status --porcelain | wc -l) -eq 0 || { git diff; false; }
